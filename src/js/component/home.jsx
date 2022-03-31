e68a6890bd7c8ba4483427c0f21269c73675c783
55746 <Lawhead.gavin@live.jobcorps.org>
2022-03-31 19:08:41 +0000

    gotta fix the delete button

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [item, setItem] = useState("");
+  const [todolist, setTodolist] = useState([""]);
+  const onRemove = (e) => {
+    setTodolist(todolist.filter((todolist) => todolist !== item));
+  };
+  return (
+    <div>
+      <div className="input-group mb-3">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Username"
+          onChange={(e) => setItem(e.target.value)}
+        />
+
+        <a
+          onClick={() => {
+            // setIte;
+            setTodolist([...todolist, item]);
+            console.log(item);
+          }}
+          type="btn btn-primary"
+          className="input-group-text"
+          id="basic-addon1"
+        >
+          Button
+        </a>
+      </div>
+      <ul>
+        {todolist.map((item, uniquekeyname) => (
+          <>
+            <li key={uniquekeyname}>
+              <div>{item}</div>
+            </li>
+            <button onClick={onRemove(uniquekeyname)}>delete</button>
+          </>
+        ))}
+      </ul>
+      <span>{todolist.length + " items"}</span>
+    </div>
+  );
 };
 
 export default Home;
