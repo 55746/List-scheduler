36a0805a7f21298db506527d20c02034af79f4c9
55746 <Lawhead.gavin@live.jobcorps.org>
2022-04-01 15:22:19 +0000

    you cant do that jimbo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,47 +1,56 @@
 import React, { useState } from "react";
 
 const Home = () => {
-  const [item, setItem] = useState("");
-  const [todolist, setTodolist] = useState([""]);
-  const onRemove = (e) => {
-    setTodolist(todolist.filter((todolist) => todolist !== item));
-  };
-  return (
-    <div>
-      <div className="input-group mb-3">
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Username"
-          onChange={(e) => setItem(e.target.value)}
-        />
+	const [item, setItem] = useState("");
+	const [todolist, setTodolist] = useState([]);
+	const works = (x) => {
+		const par = todolist.filter((element) => element !== x);
+		setTodolist(par);
+	};
+	return (
+		<div className="Container">
+			<div className="input-group mb-3">
+				<input
+					type="text"
+					className="form-control"
+					placeholder="Username"
+					onChange={(e) => setItem(e.target.value)}
+					value={item}
+				/>
 
-        <a
-          onClick={() => {
-            // setIte;
-            setTodolist([...todolist, item]);
-            console.log(item);
-          }}
-          type="btn btn-primary"
-          className="input-group-text"
-          id="basic-addon1"
-        >
-          Button
-        </a>
-      </div>
-      <ul>
-        {todolist.map((item, uniquekeyname) => (
-          <>
-            <li key={uniquekeyname}>
-              <div>{item}</div>
-            </li>
-            <button onClick={onRemove(uniquekeyname)}>delete</button>
-          </>
-        ))}
-      </ul>
-      <span>{todolist.length + " items"}</span>
-    </div>
-  );
+				<a
+					onClick={() => {
+						// setIte;
+						if (item == "") return alert("you cant do that Jimbo");
+						setTodolist([...todolist, item]);
+						setItem("");
+						console.log(item);
+					}}
+					type="btn btn-primary"
+					className="input-group-text"
+					id="basic-addon1">
+					Button
+				</a>
+			</div>
+			<ul>
+				{todolist.map((item, uniquekeyname) => {
+					return (
+						<li key={uniquekeyname} className="boxtype">
+							{item}
+							<a
+								className="ml-2 btn btn-danger"
+								onClick={() => {
+									works(item);
+								}}>
+								X
+							</a>
+						</li>
+					);
+				})}
+			</ul>
+			<span>{todolist.length + " items"}</span>
+		</div>
+	);
 };
 
 export default Home;
